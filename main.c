be1d40862b22867f603a201f357a3010c45971f2
Francois Jeannotte <francois.jeannotte@gmail.com>
2013-09-05 18:23:54 -0500

    Make balls fall with gravity.

--- main.c
+++ main.c
@@ -55,10 +55,10 @@ int update(array_obj* objs)
     assert(objs);
     for (int i = 0; i < objs->size; ++i)
     {
-        objs->data[i].location[0] += 0.1 * objs->data[i].velocity[0];
-        objs->data[i].location[1] += 0.1 * objs->data[i].velocity[1];
-        objs->data[i].location[2] += 0.1 * objs->data[i].velocity[2];
-        objs->data[i].velocity[1] += 0.1 * 9.8;
+        objs->data[i].location[0] += 0.01 * objs->data[i].velocity[0];
+        objs->data[i].location[1] += 0.01 * objs->data[i].velocity[1];
+        objs->data[i].location[2] += 0.01 * objs->data[i].velocity[2];
+        objs->data[i].velocity[1] += 0.01 * -9.8;
     }
 
     return 0;
@@ -258,7 +258,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     o.location[1] = 6.0;
     o.location[2] = 0.0;
     o.velocity[0] = 0.0;
-    o.velocity[1] = -0.2;
+    o.velocity[1] = 0.0;
     o.velocity[2] = 0.0;
     *array_obj_pushback(&a) = o;
 
@@ -286,7 +286,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
         {
             update(&a);
             draw(hDC, &a);
-            Sleep(100);
+            Sleep(1);
         }
     }
     
